bf00bf371bf5009469d4340db571c8be2a458192
Carpetoid <abdulrahmanfayoumi2@gmail.com>
2020-11-25 16:45:09 +0300

    test

--- Fib.py
+++ Fib.py
@@ -2,21 +2,12 @@
 def fib_c(n, f):
     pass
 
-<<<<<<< HEAD
-#Tail recursion implementation:
-def fib_a(n, a=[1, 1]):
-    if n == 0 or n == 1:
-        #return a =
-        pass
-    
-=======
 
 
 #Tail recursion:
 def fib_a(n, a=[0, 0, 1 ]):
     pass
->>>>>>> 0980321a7ebb18f480fc9d57c997242d508b61e7
-    
+
     
 
 
@@ -27,7 +18,8 @@ def fib_r(n):
         return 1
     return fib_r(n-1) + fib_r(n-2)
 
-
+for i in range(10):
+    print(fib_r(i))
 
 #Iterative:
 def fib_l(n):
@@ -39,7 +31,8 @@ def fib_l(n):
         previous2 = previous1
         previous1 = current
         current = previous1 +previous2
-    
     return current
 for i in range(10):
     print(fib_l(i))
+    
+  
